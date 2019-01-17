81396bc6532113a6fd98fb65628eaf7c3616b891
Johnathon Wood <jaywood128@gmail.com>
2019-01-17 23:36:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,8 +13,8 @@ def self.create_table
   sql =  <<-SQL
     CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
-      name = TEXT,
-      grade = INTEGER
+      name TEXT,
+      grade INTEGER
 
     )
     SQL
